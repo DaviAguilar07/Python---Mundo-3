7fffadb8826c0c147ccd9771de26e64319a45791
Davi Aguilar <aguilardavi44@gmail.com>
2026-09-17 17:30:37 -0300

    Aula 16 - Exemplo 1

---
+++ aula16_1.py
@@ -0,0 +1,10 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim') #Tuplas.
+
+# for cont in lanche:
+#     print(cont)
+
+for cont in range(0, len(lanche)):
+    print(lanche[cont])
+
+# for pos, comida in enumerate(lanche):
+#     print(f"Eu vou comer {comida} na posição {pos}.")
