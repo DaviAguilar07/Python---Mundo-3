7574c625bcff18f892791f67e9e3a0ba3dcac99b
Davi Aguilar <aguilardavi44@gmail.com>
2026-09-17 17:32:03 -0300

    Aula 16 - Exemplo 2

---
+++ aula16_02.py
@@ -0,0 +1,8 @@
+a = (3, 6, 8)
+b = (7, 8, 5)
+c = a + b
+
+# print(c)
+# print(c.count(8)) -> Conta a quantidade de 8 na tupla.
+# print(c.index(8)) -> Cita onde está a posição do primeiro valor procurado (8).
+# print(c.index(8, 3)) -: Cita onde está a posição do 8, após a posição 3.
